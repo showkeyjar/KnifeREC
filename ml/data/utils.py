9878b10911aaa47f42c23569c4dba677eb76da76
zergskj@163.com <zergskj@163.com>
2020-03-21 16:13:29 +0800

    add sample functions

--- ml/data/utils.py
+++ ml/data/utils.py
@@ -3,16 +3,26 @@ import pandas as pd
 
 """
 数据处理工具包
+
+2.读入数据
+3.特征工程
 """
 
 
 def create_connect():
+    """
+    建立连接
+    :return:
+    """
     sqlalchemy.create_engine("sqlite:///test.db?check_same_thread=False", echo=True)
 
 
 def read_data(data_type,data_uri):
     """
     读取数据
+    :param data_type:
+    :param data_uri:
+    :return:
     """
     sample_data = None
     if data_type == 'csv':
@@ -23,6 +33,10 @@ def read_data(data_type,data_uri):
 def extract_feature(df, columns):
     """
     特征抽取
+    :param df:
+    :param columns:
+    :return:
     """
     df_result = df[columns]
     return df_result
+
--- ml/model/utils.py
+++ ml/model/utils.py
@@ -1,4 +1,31 @@
 
 """
 todo 模型工具包
+
+4.模型训练
+5.模型部署
 """
+
+
+def train_model(model, data):
+    """
+    模型训练
+    :param model:
+    :param data:
+    :return:
+    """
+    try:
+        model.train(data)
+    except:
+        pass
+    return True
+
+
+def pub_model(model,conf):
+    """
+    模型部署
+    :param model:
+    :param conf:
+    :return:
+    """
+    pass
